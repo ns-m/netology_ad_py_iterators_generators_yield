ce3ca640961eb999cd244233f6feacacf1c82030
kmv <9629095@gmail.com>
2020-03-19 16:54:05 +0300

    Iterator

--- classes/iterator_class.py
+++ classes/iterator_class.py
@@ -1,4 +1,19 @@
 import requests
+import json
+from pprint import pprint
+
 
 class Iterator:
-    pass
+    with open('../data/countries.json', 'r') as f:
+        raw_data = json.load(f)
+    countrys = []
+    for data in raw_data:
+        countrys.append(data['name']['common'])
+    wiki_url = 'https://en.wikipedia.org/wiki/'
+    with open('../data/links.json', 'a') as flink:
+        country_links = []
+        for country in countrys:
+            country_links.append(wiki_url+country)
+    print(country_links)
+
+Iterator()
